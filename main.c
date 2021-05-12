8989ca42e8b5316ad21a9c776d3621cf78574b6c
Maria Saatgian <>
2021-05-12 03:17:11 +0000

    new conjugations
    added eastern dialect conjugation as well as differentiating between how verbs are conjugated in western if they start with a vowel or not

--- main.c
+++ main.c
@@ -8,8 +8,9 @@ char* chunk_without_two(char* verb)
 	return verb;
 }
 
-// prints present positve conjugation
-int pres_conj(char* verb, char letter)
+// prints present positve conjugation if verb starts
+//   with a vowel
+int pres_conj_vow(char* verb, char letter)
 {
   // singular
   printf("yes g'%s%cm\n", verb, letter);
@@ -20,6 +21,36 @@ int pres_conj(char* verb, char letter)
   printf("menk g'%s%cnk\n", verb, letter);
   printf("touk g'%s%ck\n", verb, letter);
   printf("anonk g'%s%cn\n", verb, letter);
+  return 0;
+}
+
+// prints present positve conjugation if verb starts
+//   with a consonant
+int pres_conj_cons(char* verb, char letter)
+{
+  // singular
+  printf("yes ge %s%cm\n", verb, letter);
+  printf("toun ge %s%cs\n", verb, letter);
+  printf("an ge %s%c\n", verb, letter);
+
+  // plural
+  printf("menk ge %s%cnk\n", verb, letter);
+  printf("touk ge %s%ck\n", verb, letter);
+  printf("anonk ge %s%cn\n", verb, letter);
+  return 0;
+}
+
+int pres_conj_east(char* verb)
+{
+  // singular
+  printf("yes %soum em\n", verb);
+  printf("dou %soum es\n", verb);
+  printf("na %soum e\n", verb);
+
+  // plural
+  printf("menk %soum enk\n", verb);
+  printf("douk %soum ek\n", verb);
+  printf("nrank %soum en\n", verb);
   return 0;
 }
 
@@ -45,7 +76,15 @@ while(1){
   char second_last = in_verb[(strlen(in_verb)-2)];
   char let_last = in_verb[(strlen(in_verb)-1)];
 
-  //getting input for tense
+  // getting input for dialect
+
+  char in_dialect[25]; 
+  char eastern_dia[25] = "eastern";
+  char western_dia[25] = "western";
+  printf("Choose a dialect:\neastern, western\n");
+  scanf("%s", in_dialect);
+
+  // getting input for tense
   
   char in_tense[25]; 
   printf("Enter a tense from the list:\npresent\n");
@@ -55,10 +94,23 @@ while(1){
   char* root = chunk_without_two(in_verb);
   char present[25] = "present";
 
-  if (strcmp(in_tense, present) == 0){
-    // calling present conugation function on input
-    pres_conj(root,second_last);
+if ((strcmp(in_dialect, western_dia) == 0)) {
+  if ((strcmp(in_tense, present) == 0) && ((in_verb[0] == 'a') || (in_verb[0] == 'e') || (in_verb[0] == 'i') || (in_verb[0] == 'o') || (in_verb[0] == 'u'))){
+    // calling present conugation function on input 
+    //   for verb starting with vowel
+    pres_conj_vow(root,second_last);
   }
+  else if ((strcmp(in_tense, present) == 0)){
+    // calling present conugation function on input
+    //  for verb starting with consonant
+    pres_conj_cons(root,second_last);
+  }
+} 
+else if ((strcmp(in_dialect, eastern_dia) == 0)){
+  if ((strcmp(in_tense, present) == 0)){
+    pres_conj_east(root);
+  }
+}
 
   //printf("\nenter 'exit' to exit the program or anything else to continue:\n");
   //scanf("%c", &check);
